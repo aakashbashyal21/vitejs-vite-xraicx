c6376dd64ca0825a9861fc0c9d8478a8efe1bcb3
aakashbashyal21 <aakashbashyal@gmail.com>
2022-05-25 11:02:24 +0545

    1 file modified

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import AppLayout from './layouts/AppLayout';
+import AppLayout from './layouts/AppLayout.vue';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
